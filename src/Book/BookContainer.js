57487ed80763c9522a8cf0b14221da71b4c25f30
github username <github email address>
2020-03-15 22:15:03 -0400

    book container for book form book search and book list

--- src/Book/BookContainer.js
+++ src/Book/BookContainer.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
+import BookForm from "./BookForm";
+import BookList from "./BookList";
+import Search from "../Search";
 
 class BookContainer extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            allBooks: [],
+            search: ''
+        }
+    }
+
+
+    componentDidMount() {
+        fetch('http://localhost:3000/books')
+            .then(db => db.json())
+            .then(bookList =>
+                this.setState({allBooks: bookList}
+            ));
+    }
+
+
+    handleSearch = (event) => {
+        this.setState( {
+            search: event.target.value
+        })
+    }
+
+    addNewBook = (newBook) => {
+        this.setState(previousState => {
+            return {allPokemon: [...previousState.allBooks, newBook]}
+        })
+    }
+
+
     render() {
         return (
             <div>
-                
+                <Search onChange={this.handleSearch}/>
+                <BookForm addNewBook={this.addNewBook}/>
+                <BookList allBooks={this.state.allBooks.filter(
+                    book => book.title.toLowerCase().includes(this.state.search.toLowerCase())
+                )}/>
             </div>
         );
     }
