2329f5e2fd7a3fd971adffaeb1bd63b197beaea0
github username <github email address>
2020-03-15 22:17:39 -0400

    holds book container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import BookContainer from "./Book/BookContainer";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+    render() {
+      return (
+          <div className="AppContainer">
+              <h1 className="header">Welcome to Your Book Collection</h1>
+              <BookContainer classNam='book-container'/>
+          </div>
+      );
+  }
 }
 
 export default App;
