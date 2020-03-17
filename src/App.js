d0e3aa74dfd7908760354fe3a06bb349624c548d
github username <github email address>
2020-03-17 03:09:20 -0400

    changed the header for the project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component{
     render() {
       return (
           <div className="AppContainer">
-              <h1 className="header">Welcome to Your Book Collection</h1>
+              <h1 className="header">The Book Collector</h1>
               <BookContainer classNam='book-container'/>
           </div>
       );
